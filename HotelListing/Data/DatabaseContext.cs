48a5f31868ebb2dfb7151ac10228cf2efe40fc60
Ayman Kheireldeen <aymanmido067@gmail.com>
2022-05-21 19:21:20 +0200

    Adding Database Configuration

---
+++ HotelListing/Data/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
